9af2a650991e95632f5e6be09a62e123da78e951
anathkantonda <anathkantonda@gmail.com>
2020-03-11 16:37:25 +0200

    used Regex for form validation and giving warning

---
+++ validation.js
@@ -0,0 +1,27 @@
+// validation script here
+var reg = /[a-z]/ig;
+// var reg2 = new RegExp(/[a-z]/, 'i');
+
+const inputs = document.querySelectorAll('input');
+
+const patterns = {
+    telephone: /^\d{10}$/,
+    username: /^[a-z\d]{5,12}$/i,
+    password: /^[\w@-]{8,20}$/,
+    slug: /^[a-z\d-]{8,20}$/,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+}
+
+function validate(field, regex){
+    if(regex.test(field.value)){
+        field.className = 'valid';
+    }else{
+        field.className = 'invalid';
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        validate(e.target, patterns[e.target.attributes.name.value])
+    });
+});
